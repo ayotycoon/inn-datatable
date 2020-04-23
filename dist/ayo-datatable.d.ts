f22d7bb20d9262eaeebb283220bbe051018aaed5
ayotycoon <38573905+ayotycoon@users.noreply.github.com>
2020-04-23 11:43:42 +0100

    first commit

--- dist/ayo-datatable.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Generated bundle index. Do not edit.
- */
-export * from './index';
-export { DatatableComponent as ɵa } from './components/datatable/datatable.component';
-export { DropdownComponent as ɵb } from './components/dropdown/dropdown.component';
--- dist/index.d.ts
+++
@@ -1,1 +0,0 @@
-export { DatatableModule } from "./datatable.module";
